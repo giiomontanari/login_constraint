a64d5c7f4303297b86b4ee2e2e32d86d74de0d77
giokaze <giovannimontanari3@gmail.com>
2020-05-22 11:14:18 -0300

    login constraint

---
+++ app/src/main/java/com/example/login_constraint/Constraint_coffee.kt
@@ -0,0 +1,12 @@
+package com.example.login_constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint_coffee : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_coffee)
+    }
+}
